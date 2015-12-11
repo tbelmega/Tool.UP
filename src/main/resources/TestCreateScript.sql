05671b219181a5d78f3831a38d80d70179ba2197
Thiemo Belmega <thiemo.belmega@uni-potsdam.de>
2015-12-11 16:06:07 +0100

    Alter schema so that categories can have supercategories

--- src/main/resources/TestCreateScript.sql
+++ src/main/resources/TestCreateScript.sql
@@ -8,10 +8,13 @@ CREATE TABLE application (
   PRIMARY KEY (uuid)
 );
 CREATE TABLE category (
-  uuid        VARCHAR(64)  NOT NULL,
-  title       VARCHAR(128) NOT NULL,
-  description LONGTEXT,
-  PRIMARY KEY (uuid)
+  uuid          VARCHAR(64)  NOT NULL,
+  title         VARCHAR(128) NOT NULL,
+  description   LONGTEXT,
+  supercategory VARCHAR(64),
+  PRIMARY KEY (uuid),
+  FOREIGN KEY (supercategory) REFERENCES category (uuid)
+    ON DELETE SET NULL
 );
 CREATE TABLE feature (
   uuid        VARCHAR(64)  NOT NULL,
@@ -41,8 +44,11 @@ CREATE TABLE application_has_feature (
 INSERT INTO application VALUES ('application-test_id_1', 'Dropbox', 'Dropbox Description');
 INSERT INTO application VALUES ('application-test_id_2', 'Box.UP', 'Box.UP Description');
 
-INSERT INTO category VALUES ('category-test_id_11', 'Cloud Speicher', 'Cloud Speicher Description');
-INSERT INTO category VALUES ('category-test_id_12', 'Wiki', 'Wiki Description');
+INSERT INTO category
+VALUES ('category-test_id_13', 'Überkategorie', 'Eine Kategorie zum Testen der Superkategorie-Beziehung', NULL);
+INSERT INTO category
+VALUES ('category-test_id_11', 'Cloud Speicher', 'Cloud Speicher Description', 'category-test_id_13');
+INSERT INTO category VALUES ('category-test_id_12', 'Wiki', 'Wiki Description', 'category-test_id_13');
 
 INSERT INTO feature VALUES ('feature-test_id_21', 'Kalender anlegen', 'Kalender anlegen Description');
 INSERT INTO feature VALUES ('feature-test_id_22', 'Termine teilen', 'Termine teilen Description');
