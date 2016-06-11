19ae8e2b84aeb268dc6ecea460cf6ebc5e049559
t.belmega@gmx.de <7B9deeyyUpjmqwNnX1Ej>
2016-06-11 11:41:37 +0200

    Remove old version of front-end

--- src/main/java/de/unipotsdam/cs/toolup/ws/resource/SearchResource.java
+++ src/main/java/de/unipotsdam/cs/toolup/ws/resource/SearchResource.java
@@ -15,6 +15,14 @@ import static de.unipotsdam.cs.toolup.util.JerseyUtil.createResponseOk;
 @Path("search")
 public class SearchResource {
 
+    /**
+     * Search for all applications, that match the search String in their title or description
+     * or the title or description of one of its features.
+     *
+     * @param searchString
+     * @return
+     * @throws Exception
+     */
     @POST
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public Response post(@FormParam("search string") String searchString) throws Exception {
